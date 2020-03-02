3e0ad54ea5be59d3f9c4d6bd9a984a0c6d8d3c5e
Khoa Nam Pham <pknam25@gmail.com>
2020-03-02 10:43:23 +0800

    Completed documentation for Simulator class

--- Simulator.java
+++ Simulator.java
@@ -1,11 +1,21 @@
+/**
+ * Author:      Khoa Nam Pham
+ * Date modified:   02/03/2020
+ * Purpose:     The main Simulator class that control the simulation.
+ */
+
 public class Simulator {
-    private static final double HZ = 0.5;
+    private static final double HZ = 0.5;  // The number of frame drawn per clock tick
 
-    private Particle[] particles;
-    private MinPQ<Event> pq;
-    private double time;
-    private int frameWidth = Draw.DEFAULT_SIZE, frameHeight = Draw.DEFAULT_SIZE;
+    private Particle[] particles;  // All the particles in the simulation
+    private MinPQ<Event> pq;  // The queue to maintain the next coming event
+    private double time;  // The elapsed time the simulation had happend
+    private int frameWidth = Draw.DEFAULT_SIZE, frameHeight = Draw.DEFAULT_SIZE;  // Canvas size
 
+    /**
+     * Create a simulator with the given number of particles.
+     * All the particles in the simulator will be generated randomly.
+     */
     public Simulator(int pNum) {
         particles = new Particle[pNum];
         for (int i = 0; i < pNum; i++) {
@@ -16,22 +26,32 @@ public class Simulator {
         pq = new MinPQ<>();
     }
 
-    public void simulate() {
+    /**
+     * Starts the simulation with the given time limit
+     * @param limit The time limit for the simulation, simulation will stop after this amount of time.
+     */
+    public void simulate(double limit) {
+        // Pre-fill the queue with all initial events
         for (int i = 0; i < particles.length; i++) 
-            predictCollission(particles[i]);
-        pq.insert(new Event(null, null, 0));
+            predictCollission(particles[i], limit);
+        pq.insert(new Event(null, null, 0));  // Draw the particles first
 
+        // While there is still an event in the queue
         while (!pq.isEmpty()) {
             Event event = pq.remove();
+        
             if (event.isValid()) {
                 Particle a = event.a;
                 Particle b = event.b;
                 double dt = event.time - time;
 
+                // Move all the particles toward the moment when the event occurs
                 for (int i = 0; i < particles.length; i++) 
                     particles[i].move(dt);
 
                 time = event.time;
+
+                // Apply approriate action according to the event
                 if (a != null && b != null)
                     a.bounceOff(b);
                 else if (a == null && b != null)
@@ -39,40 +59,81 @@ public class Simulator {
                 else if (a != null && b == null)
                     a.bounceOffVerticalWall();
                 else
-                    redraw();
+                    redraw(limit);
 
-                predictCollission(a);
-                predictCollission(b);
+                // Predict collsion for the participating particles
+                predictCollission(a, limit); 
+                predictCollission(b, limit);
             }
         }
     } 
 
-    private void predictCollission(Particle a) {
+    /**
+     * Predict all the possible collision involving the given particle
+     * @param a The particle that needs to be assessing
+     * @param limit The simulation time limit
+     */
+    private void predictCollission(Particle a, double limit) {
         if (a != null) {
+            // Particle-Particle prediction
             for (int i = 0; i < particles.length; i++) {
-                pq.insert(new Event(a, particles[i], time + a.timeToHit(particles[i])));
+                double dt = a.timeToHit(particles[i]);
+                if (dt + time < limit)
+                    pq.insert(new Event(a, particles[i], time + dt));
             }
 
-            pq.insert(new Event(a, null, time + a.timeToHitVerticalWall(frameWidth)));
-            pq.insert(new Event(null, a, time + a.timeToHitHorizontalWall(frameHeight)));
+            // Particle-Wall prediction
+            double dtX = a.timeToHitVerticalWall(frameWidth);
+            double dtY = a.timeToHitHorizontalWall(frameHeight);
+            
+            if (time + dtX < limit)
+                pq.insert(new Event(a, null, time + dtX));
+            
+                if (time + dtY < limit)
+                pq.insert(new Event(null, a, time + dtY));
         }
     }
 
-    private void redraw() {
+    /**
+     * Draw all the particles
+     * @param limit The simulation time limit
+     */
+    private void redraw(double limit) {
         Draw.clear();
         for (int i = 0;i < particles.length; i++) 
             particles[i].draw();
 
         Draw.show();
         Draw.pause(25);
-        pq.insert(new Event(null, null, time + 1 / HZ));
+        
+        if (time + 1 / HZ < limit)
+            pq.insert(new Event(null, null, time + 1 / HZ));
     }
 
+    /**
+     * This class represent an event that can happen in the simulation
+     * Each event will have:
+     * a, b: 2 particles involving in the event
+     * time: the time when the event happens
+     * countA, countB: The count number to determine if there is no event intervens the current event
+     * 
+     * The particles will define the type of the event, there are 4 types of event during the simulation:
+     * a = null, b = null: redraw event
+     * a = null, b != null: particle-horizontal wall event
+     * a != null, b = null: particle-vertical wall event
+     * a != null, b != null: particle-particle event
+     */
     private class Event implements Comparable<Event> {
         private Particle a, b;
         private int countA, countB;
         private double time;
 
+        /**
+         * Create a new event with the given particles and time
+         * @param a particle A
+         * @param b particle B
+         * @param time the time when the event happen
+         */
         public Event(Particle a, Particle b, double time) {
             this.a = a;
             this.b = b;
@@ -81,6 +142,10 @@ public class Simulator {
             this.countB = b == null ? -1 : b.count();
         }
 
+        /**
+         * Checks if an event is valid, it is valid if no other events intervens it.
+         * @return true if valid or false otherwise
+         */
         public boolean isValid() {
             if (a != null && countA != a.count()) return false;
             if (b != null && countB != b.count()) return false;
@@ -94,9 +159,18 @@ public class Simulator {
         }
     }
 
+    // Test client
     public static void main(String[] args) {
-        Draw.enableDoubleBuffering();
-        Simulator simulator = new Simulator(10);
-        simulator.simulate();
+        if (args.length < 2)
+            System.out.println("Please specify the number of particles and the simulation time");
+        else {
+            int pNum = Integer.parseInt(args[0]);
+            double limit = Double.parseDouble(args[1]);
+            
+            Draw.enableDoubleBuffering();
+            
+            Simulator simulator = new Simulator(pNum);
+            simulator.simulate(limit);
+        }
     }
 }
